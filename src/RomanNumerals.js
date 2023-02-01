a49186d197b8547fb5b283aaff22d13a8bb32b00
Matthew Hanson <dingbat91@gmail.com>
2023-02-01 16:12:13 +0000

    Initial Output Test

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,11 +1,22 @@
 export function RomanNumerals(...nums) {
 	const LOOKUP = [
-		{ numeral: "I", decimal: 1 },
-		{ numeral: "V", decimal: 5 },
-		{ numeral: "X", decimal: 10 },
-		{ numeral: "L", decimal: 50 },
 		{ numeral: "C", decimal: 100 },
+		{ numeral: "L", decimal: 50 },
+		{ numeral: "X", decimal: 10 },
+		{ numeral: "V", decimal: 5 },
+		{ numeral: "I", decimal: 1 },
 	];
+	let output = 0;
 
-	return ["I"];
+	if (!nums) {
+		throw new error("Missing numbers");
+	}
+
+	nums.forEach((num) => {
+		if (num === 1) {
+			output = ["I"];
+		}
+	});
+
+	return output;
 }
