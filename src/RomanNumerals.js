f9431ef33577136a14880130ad8d2eaef3fdfec7
Matthew Hanson <dingbat91@gmail.com>
2023-02-03 16:43:49 +0000

    Initial Functionality

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,21 +1,44 @@
 export function RomanNumerals(...nums) {
 	const LOOKUP = [
 		{ numeral: "C", decimal: 100 },
+		{ numeral: "XC", decimal: 90 },
 		{ numeral: "L", decimal: 50 },
+		{ numeral: "XL", decimal: 40 },
 		{ numeral: "X", decimal: 10 },
+		{ numeral: "IX", decimal: 9 },
 		{ numeral: "V", decimal: 5 },
+		{ numeral: "IV", decimal: 4 },
 		{ numeral: "I", decimal: 1 },
 	];
-	let output = 0;
 
-	if (!nums) {
+	let output = [];
+
+	if (nums.length === 0) {
 		throw new error("Missing numbers");
 	}
 
 	nums.forEach((num) => {
-		if (num === 1) {
-			output = ["I"];
+		let numeralString = "";
+
+		for (let currentTotal = num; currentTotal > 0; ) {
+			const REDUCINGNUMERAL = LOOKUP.find((numeral) => {
+				//subraction variable (refactor?)
+				const subtraction = currentTotal - numeral.decimal;
+
+				//Returns true if subtraction does not result in a negative number.
+				if (subtraction < 0) {
+					return false;
+				} else {
+					//subtract found decimal value for next iteration
+					currentTotal -= numeral.decimal;
+					return true;
+				}
+			});
+
+			//construct string
+			numeralString += REDUCINGNUMERAL.numeral;
 		}
+		output.push(numeralString);
 	});
 
 	return output;
--- test/RomanNumerals.test.js
+++ test/RomanNumerals.test.js
@@ -1,6 +1,46 @@
 import { RomanNumerals } from "../src/RomanNumerals";
 
-test("should check if 1 turns to I", () => {
-	const results = RomanNumerals(1);
-	expect(results).toStrictEqual(["I"]);
+describe("Single Number Tests", () => {
+	test("should check if 1 turns to I", () => {
+		const results = RomanNumerals(1);
+		expect(results).toStrictEqual(["I"]);
+	});
+
+	test("should check if 5 turns to V", () => {
+		const results = RomanNumerals(5);
+		expect(results).toStrictEqual(["V"]);
+	});
+
+	test("should check if 8 turns to VIII", () => {
+		const results = RomanNumerals(8);
+		expect(results).toStrictEqual(["VIII"]);
+	});
+
+	test("should check if 9 turns to IX", () => {
+		const results = RomanNumerals(9);
+		expect(results).toStrictEqual(["IX"]);
+	});
+
+	test("should check if 45 turns to X", () => {
+		const results = RomanNumerals(45);
+		expect(results).toStrictEqual(["XLV"]);
+	});
+});
+
+describe("Multi Number Tests", () => {
+	test('should turn 5 and 8 into ["V", "VIII"]', () => {
+		const results = RomanNumerals(5, 8);
+		expect(results).toStrictEqual(["V", "VIII"]);
+	});
+
+	test('should turn 5 and 8 into ["V", "VIII"]', () => {
+		const results = RomanNumerals(28, 72);
+		expect(results).toStrictEqual(["XXVIII", "LXXII"]);
+	});
+});
+
+describe("Input Tests", () => {
+	test("No input error check", () => {
+		expect(() => RomanNumerals()).toThrow(Error);
+	});
 });
